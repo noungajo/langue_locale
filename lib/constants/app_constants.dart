e14fec38c2d1a7509c65d8b761d7d9f202c0c3c6
jonathan <jonathan@ofty.com>
2023-11-13 13:20:56 +0100

    ajout de nouvelles constantes

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -3,6 +3,20 @@
  languePrisEnCharge =  [
     "Fè'éfè'é",
     "Douala",
-  ]
- 
+  ],
+ audioUrlBaseCompter = "audios/compter/",
+ audioUrlBasefruit = "audios/fruits/",
+ audioUrlBaseAnimal = "audios/animaux/",
+ audioUrlBaseMoisDeLanne = "audios/mois_annee/",
+ audioUrlBasePremierMot = "audios/premier_mot/",
+ audioUrlBasePresentation = "audios/presentation/",
+ audioUrlBaseSalutation = "audios/salutation/",
+
+ imageUrlBaseCompter = "assets/images/compter/",
+ imageUrlBasefruit = "assets/images/fruits/",
+ imageUrlBaseAnimal = "assets/images/animaux/",
+ imageUrlBaseMoisDeLanne = "assets/images/mois_annee/",
+ imageUrlBasePremierMot = "assets/images/premier_mot/",
+ imageUrlBasePresentation = "assets/images/presentation/",
+ imageUrlBaseSalutation = "assets/audios/salutation/"
  ;
