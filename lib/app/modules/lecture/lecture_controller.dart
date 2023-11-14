96f1adc7202497e106599be1904d488c1865570d
jonathan <jonathan@ofty.com>
2023-11-14 17:13:02 +0100

    ajout de la variable makePop dans le controller de la lecture pour gérer la navigation entre dialogue et home

--- lib/app/modules/lecture/lecture_controller.dart
+++ lib/app/modules/lecture/lecture_controller.dart
@@ -4,11 +4,19 @@ import 'lecture_modele.dart';
 
 class LectureController extends GetxController{
   RxList<LectureModele> listeLecture = RxList<LectureModele>.empty();
-
+  bool makePop = false;
   setCurrentListeLecture(List<LectureModele> listes){
     listeLecture(listes);
   }
  List<LectureModele> getCurrentListeLecture(){
    return listeLecture ;
   }
+  // fonction pour déterminer si on fait un popup ou un retour sur home
+  setPop(bool value){
+    makePop = value;
+  }
+  getPop(){
+    return makePop;
+  }
+
 }
