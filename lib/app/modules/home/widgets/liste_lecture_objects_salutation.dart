4b47f5e7ede33b2d049ef84408e5c6060932e5c0
jonathan <jonathan@ofty.com>
2023-11-14 17:11:45 +0100

    ajout du squelette des autres langues prises en charge par le système concernant la rubrique salutation

--- lib/app/modules/home/widgets/liste_lecture_objects_salutation.dart
+++ lib/app/modules/home/widgets/liste_lecture_objects_salutation.dart
@@ -4,16 +4,25 @@ import 'package:reenamuna/app/modules/lecture/lecture_modele.dart';
 import '../../../../constants/app_constants.dart';
 
 var listeLectureSalutation = [
-  LectureModele(text: "zero", imageUrl: "${imageUrlBaseCompter}0.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}0_nufi.ogg","Douala":"${audioUrlBaseCompter}0_dla.ogg"}),
-  LectureModele(text: "un", imageUrl: "${imageUrlBaseCompter}1.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}1_nufi.ogg","Douala":"${audioUrlBaseCompter}1_dla.ogg"}),
-  LectureModele(text: "deux", imageUrl: "${imageUrlBaseCompter}2.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}2_nufi.ogg","Douala":"${audioUrlBaseCompter}2_dla.ogg"}),
-  LectureModele(text: "trois", imageUrl: "${imageUrlBaseCompter}3.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}3_nufi.ogg","Douala":"${audioUrlBaseCompter}3_dla.ogg"}),
-  LectureModele(text: "quatre", imageUrl: "${imageUrlBaseCompter}4.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}4_nufi.ogg","Douala":"${audioUrlBaseCompter}4_dla.ogg"}),
-  LectureModele(text: "cinq", imageUrl: "${imageUrlBaseCompter}5.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}5_nufi.ogg","Douala":"${audioUrlBaseCompter}5_dla.ogg"}),
-  LectureModele(text: "six", imageUrl: "${imageUrlBaseCompter}6.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}6_nufi.ogg","Douala":""}),
-  LectureModele(text: "sept", imageUrl: "${imageUrlBaseCompter}7.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}7_nufi.ogg","Douala":""}),
-  LectureModele(text: "huit", imageUrl: "${imageUrlBaseCompter}8.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}8_nufi.ogg","Douala":""}),
-  LectureModele(text: "neuf", imageUrl: "${imageUrlBaseCompter}9.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}9_nufi.ogg","Douala":""}),
-  LectureModele(text: "dix", imageUrl: "${imageUrlBaseCompter}10.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseCompter}10_nufi.ogg","Douala":""}),
+   //LectureModele(text: "1_lonam", imageUrl: "${imageUrlBaseSalutation}2-matin_lonam.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}1-bonjour_grande_soeur_mbouani_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "2_lonam", imageUrl: "${imageUrlBaseSalutation}2-matin_lonam.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}2-bonjour_grande_soeur_mbouani_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "3_mbouani", imageUrl: "${imageUrlBaseSalutation}3-matin_mbouani.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}3-bonjour_mon_petit_frere_lonam_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "4_mbouani", imageUrl: "${imageUrlBaseSalutation}3-matin_mbouani.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}4-comment_ca_va_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "5_lonam", imageUrl: "${imageUrlBaseSalutation}4-matin_lonam_2.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}5-ca_va_bien_merci_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "6_lonam", imageUrl: "${imageUrlBaseSalutation}5-salut_papa_lonam.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}6-salut_papa_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "7_papa", imageUrl: "${imageUrlBaseSalutation}6-bonjour_mon_enfant.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}7-salut_mon_enfant_comment_ca_va_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "8_lonam", imageUrl: "${imageUrlBaseSalutation}5-salut_papa_lonam.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}8-ca_va_bien_papa_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "9_lonam", imageUrl: "${imageUrlBaseSalutation}5-salut_papa_lonam.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}9-et_toi_alors_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "10_papa", imageUrl: "${imageUrlBaseSalutation}6-bonjour_mon_enfant.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}10-ca_va_bien_mon_enfant_merci_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "11_mbouani", imageUrl: "${imageUrlBaseSalutation}12-bonsoir_mbouani.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}11-bonsoir_maman_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "12_maman", imageUrl: "${imageUrlBaseSalutation}13-la_mere.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}12-bonsoir_mon_enfant_comment_ca_va_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
 
+   LectureModele(text: "13_mbouani", imageUrl: "${imageUrlBaseSalutation}12-bonsoir_mbouani.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}113-ca_va_bien_maman_et_toi_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "14_maman", imageUrl: "${imageUrlBaseSalutation}13-la_mere.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}14-ca_va_bien_mon_enfant_merci_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "15_mbouani", imageUrl: "${imageUrlBaseSalutation}14-bonsoir_frere.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}15-bonne_nuit_mon_frere_lonam_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "16_lonam", imageUrl: "${imageUrlBaseSalutation}14-bonsoir_frere.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}16-bonne_nuit_ma_soeur_mbouani_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "17_enfants", imageUrl: "${imageUrlBaseSalutation}11-bonsoir_les_parents.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}17-bonne_nuit_papa_bonne_nuit_maman_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "18_parents", imageUrl: "${imageUrlBaseSalutation}11-bonsoir_les_parents.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}18-nous_vous_remercions_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "19_parents", imageUrl: "${imageUrlBaseSalutation}11-bonsoir_les_parents.png",audioMap: {"Fè'éfè'é":"${audioUrlBaseSalutation}19-bonne_nuit_a_vous_mes_enfants_demain_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+ 
   ];
