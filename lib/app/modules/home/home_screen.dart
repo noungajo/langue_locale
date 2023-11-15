274ed10295ebc8a13c4261ddaa68808cc3913849
jonathan <jonathan@ofty.com>
2023-11-15 17:35:51 +0100

    modification de l'accueil pour centrer les éléments

--- lib/app/modules/home/home_screen.dart
+++ lib/app/modules/home/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:reenamuna/app/modules/home/widgets/liste_lecture_objects_mois.dart';
-import 'package:reenamuna/app/modules/home/widgets/liste_lecture_objects_premier_mot.dart';
 import 'package:sizer/sizer.dart';
 import '../../../constants/size.dart';
 import '../../../constants/string.dart';
@@ -14,6 +12,8 @@ import 'widgets/home_item.dart';
 import 'widgets/liste_lecture_objects_animaux.dart';
 import 'widgets/liste_lecture_objects_compter.dart';
 import 'widgets/liste_lecture_objects_fruit.dart';
+import 'widgets/liste_lecture_objects_mois.dart';
+import 'widgets/liste_lecture_objects_premier_mot.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -34,52 +34,75 @@ class _HomeScreenState extends State<HomeScreen> {
         return false;
       },
       child: Scaffold(
+        
         appBar: appBar("nom_projet".tr,context),
-        drawer: SizedBox(
+      drawer: SizedBox(
           width: drawerWidth,
           child: drawer(context)),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.05),
-            child: Wrap(
-              spacing:8.w,
-              children: [
-              HomeItem(lottieIcon: lottiePremierMot,textDecoration: 'premier_mot'.tr,callback:(){
-                  lectureController.setCurrentListeLecture(listeLecturePremierMot);
+        body:SingleChildScrollView(
+  child: Container(
+    padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.05),
+    child: Wrap(
+      alignment: WrapAlignment.center, // Alignement des éléments au centre
+      spacing: 8.w,
+      children: [
+        HomeItem(
+          lottieIcon: lottiePremierMot,
+          textDecoration: 'premier_mot'.tr,
+          callback: () {
+            lectureController.setCurrentListeLecture(listeLecturePremierMot);
             lectureController.setPop(false);
-               Get.off(() =>  Lecture(titre:'premier_mot'));
-              }),
-              HomeItem(lottieIcon: lottieDialogue,textDecoration: 'dialogue'.tr,callback:(){
-                Get.toNamed("/dialog");
-              }),
-           
-            HomeItem(lottieIcon: lottieAnimaux,textDecoration: 'animaux'.tr,callback:(){
-              lectureController.setCurrentListeLecture(listeLectureAnimaux);
-
-            lectureController.setPop(false);
-               Get.off(() =>  Lecture(titre:'animaux'));
-            }),
-            HomeItem(lottieIcon: lottieNombre,textDecoration: 'compter'.tr,callback:(){
-              lectureController.setCurrentListeLecture(listeLectureNombre);
-            lectureController.setPop(false);
-             Get.off(() =>  Lecture(titre:'compter'))
-             ;}),
-           HomeItem(lottieIcon: lottieFruit,textDecoration: 'fruit'.tr,callback:(){
-              lectureController.setCurrentListeLecture(listeLectureFruit);
-            lectureController.setPop(false);
-              Get.off(() =>  Lecture(titre:'fruit'));
-           }),
-            HomeItem(lottieIcon: lottieMoisAnnee,textDecoration: 'mois'.tr,callback:(){
-                lectureController.setCurrentListeLecture(listeLectureMois);
-            lectureController.setPop(false);
-              Get.off(() =>  Lecture(titre:'mois'));
-            }),
-          
-       
-            ]),
-          ),
+            Get.off(() => Lecture(titre: 'premier_mot'));
+          },
         ),
-      ),
+        HomeItem(
+          lottieIcon: lottieDialogue,
+          textDecoration: 'dialogue'.tr,
+          callback: () {
+            Get.toNamed("/dialog");
+          },
+        ),
+        HomeItem(
+          lottieIcon: lottieAnimaux,
+          textDecoration: 'animaux'.tr,
+          callback: () {
+            lectureController.setCurrentListeLecture(listeLectureAnimaux);
+            lectureController.setPop(false);
+            Get.off(() => Lecture(titre: 'animaux'));
+          },
+        ),
+        HomeItem(
+          lottieIcon: lottieNombre,
+          textDecoration: 'compter'.tr,
+          callback: () {
+            lectureController.setCurrentListeLecture(listeLectureNombre);
+            lectureController.setPop(false);
+            Get.off(() => Lecture(titre: 'compter'));
+          },
+        ),
+        HomeItem(
+          lottieIcon: lottieFruit,
+          textDecoration: 'fruit'.tr,
+          callback: () {
+            lectureController.setCurrentListeLecture(listeLectureFruit);
+            lectureController.setPop(false);
+            Get.off(() => Lecture(titre: 'fruit'));
+          },
+        ),
+        HomeItem(
+          lottieIcon: lottieMoisAnnee,
+          textDecoration: 'mois'.tr,
+          callback: () {
+            lectureController.setCurrentListeLecture(listeLectureMois);
+            lectureController.setPop(false);
+            Get.off(() => Lecture(titre: 'mois'));
+          },
+        ),
+      ],
+    ),
+  ),
+),
+  ),
     );
   }
 }
