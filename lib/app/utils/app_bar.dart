351237ddc5e9da20fbf9d787decf5923414c1826
jonathan <jonathan@ofty.com>
2023-11-22 17:41:07 +0100

    ajout du text bouton sur l'appbar

--- lib/app/utils/app_bar.dart
+++ lib/app/utils/app_bar.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import '../../constants/colors.dart';
 import '../../constants/style.dart';
 
-AppBar appBar(String title,BuildContext context){
+AppBar appBar(RxString title,BuildContext context,Widget changeLanguageButton){
   return AppBar(
     backgroundColor: lightAppbarColor,
-    title: Text(title,style:titleMediumStyle),
+    title: Obx(()=> Text(title.value.tr,style:titleMediumStyle)),
     centerTitle: true,
+    actions: [
+
+changeLanguageButton
+
+    ],
     );
 }
+ 
+  
