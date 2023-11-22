e5155ae7410a648d937846f3ce1dff9a72522f5a
jonathan <jonathan@ofty.com>
2023-11-22 17:26:05 +0100

    modification de la structure du code de dialogue

--- lib/app/modules/dialogue/dialogue.dart
+++ lib/app/modules/dialogue/dialogue.dart
@@ -6,6 +6,7 @@ import '../../../constants/size.dart';
 import '../../../constants/string.dart';
 import '../../../constants/style.dart';
 import '../../utils/app_bar.dart';
+import '../../utils/changeLanguageText.dart';
 import '../home/widgets/drawer.dart';
 import '../home/widgets/liste_lecture_objects_presentation.dart';
 import '../home/widgets/liste_lecture_objects_salutation.dart';
@@ -19,42 +20,51 @@ class Dialogue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar("dialogue".tr,context),
-      drawer: SizedBox(
-          width: drawerWidth,
-          child: drawer(context)),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.05),
-          child: Column(
-            children: [
-         Padding(
-           padding:  EdgeInsets.only(bottom:itemSpacer,top: itemSpacer),
-           child: ListTile(
-              leading: Image.asset(dialogPresentationIcon,),
-              title:  Text('presentation_dialog'.tr,style: bodyLargeStyle,),
-              onTap: () {
-                      lectureController.setCurrentListeLecture(listeLecturePresentation);
-                      lectureController.setPop(true);
-             Get.to(() =>  Lecture(titre:'presentation_dialog'));
-              },
-            ),
-         ),
-        
-            Padding(
-              padding: EdgeInsets.only(bottom:itemSpacer,top: itemSpacer),
-              child: ListTile(
-              leading: SvgPicture.asset(dialogSalutationIcon,),
-              title:  Text('salutation_dialog'.tr,style: bodyLargeStyle,),
-              onTap: () {
-                   lectureController.setCurrentListeLecture(listeLectureSalutation);
-                      lectureController.setPop(true);
-             Get.to(() =>  Lecture(titre:'salutation_dialog'));
-              },
-                      ),
-            ),
-          ]),
+    return WillPopScope(
+         onWillPop: () async{
+        // Lorsque vous souhaitez quitter l'application, par exemple dans un bouton de votre interface utilisateur, vous pouvez appeler :
+        Get.offAllNamed("/home");
+        return false;
+      },
+      child: Scaffold(
+        appBar: appBar("dialogue".obs,context,changeLanguageButton()),
+        drawer: SizedBox(
+            width: drawerWidth,
+            child: drawer(context)),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.width*0.05),
+            child: Column(
+              children: [
+           Padding(
+             padding:  EdgeInsets.only(bottom:itemSpacer,top: itemSpacer),
+             child: ListTile(
+                leading: Image.asset(dialogPresentationIcon,),
+                title:  Text('presentation_dialog'.tr,style: bodyLargeStyle,),
+                onTap: () {
+                        lectureController.setCurrentListeLecture(listeLecturePresentation);
+                  lectureController.setCurrentTitle('presentation_dialog');
+                        lectureController.setPop(true);
+               Get.to(() =>  Lecture());
+                },
+              ),
+           ),
+          
+              Padding(
+                padding: EdgeInsets.only(bottom:itemSpacer,top: itemSpacer),
+                child: ListTile(
+                leading: SvgPicture.asset(dialogSalutationIcon,),
+                title:  Text('salutation_dialog'.tr,style: bodyLargeStyle,),
+                onTap: () {
+                     lectureController.setCurrentListeLecture(listeLectureSalutation);
+                  lectureController.setCurrentTitle('salutation_dialog');
+                        lectureController.setPop(true);
+               Get.to(() =>  Lecture());
+                },
+                        ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
