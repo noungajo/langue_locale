22df228389b19d971be25fceedeca91e4fde2a38
jonathan <jonathan@ofty.com>
2023-11-22 17:36:24 +0100

    mise à jour du controller de l'interface lecture

--- lib/app/modules/lecture/lecture_controller.dart
+++ lib/app/modules/lecture/lecture_controller.dart
@@ -4,12 +4,21 @@ import 'lecture_modele.dart';
 
 class LectureController extends GetxController{
   RxList<LectureModele> listeLecture = RxList<LectureModele>.empty();
+  RxInt currentObjet = 0.obs;
+  RxString currentTitle = "".obs;
   bool makePop = false;
   setCurrentListeLecture(List<LectureModele> listes){
     listeLecture(listes);
+    currentObjet = 0.obs;
   }
  List<LectureModele> getCurrentListeLecture(){
    return listeLecture ;
+  }
+  setCurrentTitle(String title){
+    currentTitle.value = title;
+  }
+  RxString getCurrentTitle(){
+    return currentTitle;
   }
   // fonction pour déterminer si on fait un popup ou un retour sur home
   setPop(bool value){
