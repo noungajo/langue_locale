1ee81a1ccd227874f69183d1499c3b6f6c760413
jonathan <jonathan@ofty.com>
2023-11-22 17:31:20 +0100

    externalisation des objets de la liste des fruits

--- lib/app/modules/home/widgets/liste_lecture_objects_fruit.dart
+++ lib/app/modules/home/widgets/liste_lecture_objects_fruit.dart
@@ -1,23 +1,31 @@
 
-import 'package:reenamuna/app/modules/lecture/lecture_modele.dart';
 
 import '../../../../constants/app_constants.dart';
+import '../../lecture/lecture_modele.dart';
 
+/*
+0 -> "Fè'éfè'é"
+1 -> "Douala"
+2 -> "Bangangté"
+3 -> "Yambassa"
+4 ->  "Étón"
+*/
+int indexNufi = 0,indexDouala = 1, indexBangante = 2, indexYambassa = 3, indexEton = 4;
 var listeLectureFruit = [
-  LectureModele(text: "ananas", imageUrl: "${imageUrlBasefruit}ananas.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}ananas_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "avocat", imageUrl: "${imageUrlBasefruit}avocat.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}avocat_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "banane", imageUrl: "${imageUrlBasefruit}banane.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}banane_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "citron", imageUrl: "${imageUrlBasefruit}citron.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}citron_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "corossol", imageUrl: "${imageUrlBasefruit}corossol.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}corossol_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "goyave", imageUrl: "${imageUrlBasefruit}goyave.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}goyave_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "mangue", imageUrl: "${imageUrlBasefruit}mangue.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}mangue_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "noix_de_coco", imageUrl: "${imageUrlBasefruit}noix_de_coco.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}noix_de_coco_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "orange", imageUrl: "${imageUrlBasefruit}orange.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}orange_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "palmiste", imageUrl: "${imageUrlBasefruit}palmiste.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}palmiste_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "papaye", imageUrl: "${imageUrlBasefruit}papaye.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}papaye_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "ananas", imageUrl: "${imageUrlBasefruit}ananas.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}ananas_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "avocat", imageUrl: "${imageUrlBasefruit}avocat.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}avocat_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "banane", imageUrl: "${imageUrlBasefruit}banane.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}banane_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "citron", imageUrl: "${imageUrlBasefruit}citron.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}citron_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "corossol", imageUrl: "${imageUrlBasefruit}corossol.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}corossol_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "goyave", imageUrl: "${imageUrlBasefruit}goyave.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}goyave_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "mangue", imageUrl: "${imageUrlBasefruit}mangue.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}mangue_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "noix_de_coco", imageUrl: "${imageUrlBasefruit}noix_de_coco.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}noix_de_coco_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "orange", imageUrl: "${imageUrlBasefruit}orange.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}orange_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "palmiste", imageUrl: "${imageUrlBasefruit}palmiste.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}palmiste_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "papaye", imageUrl: "${imageUrlBasefruit}papaye.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}papaye_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
 
-  LectureModele(text: "pomme_de_terre", imageUrl: "${imageUrlBasefruit}pomme_de_terre.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}pomme_de_terre_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "prune", imageUrl: "${imageUrlBasefruit}prune.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}prune_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
-  LectureModele(text: "tomate", imageUrl: "${imageUrlBasefruit}tomate.jpeg",audioMap: {"Fè'éfè'é":"${audioUrlBasefruit}tomate_nufi.ogg","Douala":"","Bangangté":"","Yambassa":"","Étón":""}),
+  LectureModele(text: "pomme_de_terre", imageUrl: "${imageUrlBasefruit}pomme_de_terre.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}pomme_de_terre_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "prune", imageUrl: "${imageUrlBasefruit}prune.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}prune_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
+  LectureModele(text: "tomate", imageUrl: "${imageUrlBasefruit}tomate.jpeg",audioMap: {languePrisEnCharge[indexNufi]:"${audioUrlBasefruit}tomate_nufi.ogg",languePrisEnCharge[indexDouala]:"",languePrisEnCharge[indexBangante]:"",languePrisEnCharge[indexYambassa]:"",languePrisEnCharge[indexEton]:""}),
   
 ];
