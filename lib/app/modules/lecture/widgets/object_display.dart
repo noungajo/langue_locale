3494174d93bf55e5f0451bc34ac9553206705cea
jonathan <jonathan@ofty.com>
2023-11-29 13:17:44 +0100

    ajout de l'animation lors de la navigation

--- lib/app/modules/lecture/widgets/object_display.dart
+++ lib/app/modules/lecture/widgets/object_display.dart
@@ -1,7 +1,7 @@
 import 'package:Reenamuna/app/modules/lecture/lecture_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:lottie/lottie.dart';
 import 'package:sizer/sizer.dart';
 
 import '../../../../constants/size.dart';
@@ -11,60 +11,71 @@ import '../lecture_modele.dart';
 
 // ignore: must_be_immutable
 class ObjectDisplay extends StatelessWidget {
-   ObjectDisplay({
+  ObjectDisplay({
     super.key,
     required this.objectList,
-    //required this.currentObjet,
+    // required this.currentObjet,
   });
 
   final List<LectureModele> objectList;
- // final RxInt currentObjet;
-var lectureController = Get.find<LectureController>();
+  // final RxInt currentObjet;
+  var lectureController = Get.find<LectureController>();
+
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      ()=> Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-      Text(
-        objectList[lectureController.currentObjet.value].text.tr ,
-        style: itemStyle,
-      ),
-      SizedBox(
-        height: itemSpacer,
-      ),
-      Card(
-        elevation: 10,
-        child: objectList[lectureController.currentObjet.value].imageUrl.isEmpty?
-        SizedBox(
-          width: lectureImageSizeHeight ,
-          height: lectureImageSizeHeight ,
-          child: Center(child: Text(objectList[lectureController.currentObjet.value].text.tr,style: openTitleStyle,)))
-        :Image.asset(
-          objectList[lectureController.currentObjet.value].imageUrl,
-          width: lectureImageSizeHeight ,
-          height: lectureImageSizeHeight ,
-          fit: BoxFit
-              .contain, // Ajuste l'image pour remplir l'espace sans
-          errorBuilder: (context, error, stackTrace) {
-            // Gérez l'erreur ici, par exemple, en affichant une image de remplacement ou un message d'erreur
-            return Image.asset(
-                imageNotFound,fit: BoxFit
-              .contain, ); // Image de remplacement
-          },
-        ),
-      ),
-     
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Lottie.asset(lottieListining, height: 10.h,
-           fit: BoxFit.scaleDown),
-          Text("ecouter".tr,style: clickTextStyle,),
-        ],
-      )
-      ],
-      ),
-    );
+    return Obx(() => AnimatedSwitcher(
+        duration: Duration(milliseconds: 500), // Réglez la durée de l'animation selon vos préférences
+              transitionBuilder: (Widget child, Animation<double> animation)=>ScaleTransition(child: child,scale:animation),
+      child: Column(  
+        key: Key(objectList[lectureController.currentObjet.value].text.tr), // Utilisez la valeur actuelle comme clé pour identifier le widget
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                objectList[lectureController.currentObjet.value].text.tr,
+                style: itemStyle,
+              ),
+              SizedBox(
+                height: itemSpacer,
+              ),
+              Card(
+                elevation: 10,
+                child: objectList[lectureController.currentObjet.value].imageUrl.isEmpty
+                    ? SizedBox(
+                        width: lectureImageSizeHeight,
+                        height: lectureImageSizeHeight,
+                        child: Center(
+                          child: Text(
+                            objectList[lectureController.currentObjet.value].text.tr,
+                            style: openTitleStyle,
+                          ),
+                        ),
+                      )
+                    : Image.asset(
+                        objectList[lectureController.currentObjet.value].imageUrl,
+                        width: lectureImageSizeHeight,
+                        height: lectureImageSizeHeight,
+                        fit: BoxFit.contain,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset(
+                            imageNotFound,
+                            fit: BoxFit.contain,
+                          );
+                        },
+                      ),
+              ),
+              SizedBox(height: 1.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                
+                children: [
+                 // Lottie.asset(lottieListining, height: 10.h, fit: BoxFit.scaleDown),
+                 SvgPicture.asset(speaker,width: iconSize*0.8),
+                 SizedBox(width: 5.w,),
+                  Text("ecouter".tr, style: clickTextStyle),
+                ],
+              )
+            ],
+          ),
+    ));
   }
 }
